a541e9fcef1010e1495b6589bcf6617a5ef13d87
alyanser <staiz7071@gmail.com>
2021-09-02 01:50:34 +0500

    script dir fix

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-[[ -d "build" ]] && cd build && [[ -x "tcpserver" ]] && ./tcpserver || echo executable not built/found
+[[ -d "../build" ]] && cd ../build && [[ -x "tcpserver" ]] && ./tcpserver || echo executable not built/found
